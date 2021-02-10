ef47bfbecfb2d2f000a0c5ac1f4c931587b126c3
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2021-02-10 15:18:25 -0600

    set up views and html files

--- ReservME/models.py
+++ ReservME/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class GuestInfo(models.Model):
+class GuestandPaymentInfo(models.Model):
     room_type = models.CharField(max_length=250)
     room_id = models.PositiveIntegerField()
     availability = models.BooleanField()
@@ -13,9 +13,6 @@ class GuestInfo(models.Model):
     zipcode = models.PositiveIntegerField()
     state = models.CharField(max_length=2)
     phone = models.PositiveIntegerField()
-
-
-class PaymentInfo(models.Model):
     company = models.CharField(max_length=250)
     card_number = models.PositiveIntegerField()
     csv = models.PositiveIntegerField()
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from ReservME.models import GuestandPaymentInfo
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class InformationView(DetailView):
+    model = GuestandPaymentInfo
+    template_name = 'information.html'
+    fields = ['room_type', 'room_id', 'availability', 'full_name', 'address', 'email', 'zipcode', 'state', 'phone', 'company', 'card_number', 'csv', 'expiration', 'card_holder']
+    
